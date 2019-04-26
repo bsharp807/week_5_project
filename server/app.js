57a0563a96baf54402e78e0edd464d06d7821daa
bsharp807 <a0030619@0030619s-MBP.me>
2019-04-26 16:45:15 +0100

    server: updated app.js with cors and body-parser

--- server/app.js
+++ server/app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/npc');
 
 var app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
