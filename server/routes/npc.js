23f961de5116fc2d7ba9c269c01b410b2918eb8e
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-04-26 17:00:30 +0100

    server: added get single char function

--- server/routes/npc.js
+++ server/routes/npc.js
@@ -2,12 +2,22 @@ var express = require('express');
 var router = express.Router();
 var SqlRunner = require('../db/sql_runner');
 
-/* GET users listing. */
+// GET all characters
 router.get('/', function(req, res) {
   SqlRunner.run('SELECT * FROM non_player_chars')
     .then((char) => {
       res.status(200).json(char.rows);
     });
 });
+
+//GET single character
+router.get('/:id', function(req, res) {
+  SqlRunner.run('SELECT * FROM non_player_chars WHERE id = $1', [req.params.id])
+    .then((char) => {
+      res.status(200).json(char.rows);
+    });
+});
+
+
 
 module.exports = router;
