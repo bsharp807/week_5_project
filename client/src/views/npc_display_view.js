d42d85ddd48963fc4c2bc99e153bd3d3b09b3e5d
bsharp807 <a0030619@0030619s-MBP.home>
2019-04-28 17:29:09 +0100

    added html forms, updated css, added more elements to display

--- client/src/views/npc_display_view.js
+++ client/src/views/npc_display_view.js
@@ -30,16 +30,35 @@ class NpcDisplayView {
     const card = document.createElement('div');
     card.classList.add('display-card');
 
+    const image = document.createElement('img');
+    image.classList.add('display-image');
+
     const header = document.createElement('div');
     header.classList.add('display-header');
     header.innerHTML = `${data.first_name} ${data.last_name}`;
+
+    const race = document.createElement('div');
+    race.classList.add('display-race');
+    race.innerHTML = data.race;
 
     const job = document.createElement('div');
     job.classList.add('display-job');
     job.innerHTML = data.job;
 
+    const highSkill = document.createElement('div');
+    highSkill.classList.add('display-highSkill');
+    highSkill.innerHTML = data.high_skill;
+
+    const lowSkill = document.createElement('div');
+    lowSkill.classList.add('display-lowSkill');
+    lowSkill.innerHTML = data.low_skill;
+
+    card.appendChild(image);
     card.appendChild(header);
+    card.appendChild(race);
     card.appendChild(job);
+    card.appendChild(highSkill);
+    card.appendChild(lowSkill);
 
     return card;
   }
