da2e51ade0e4d7f5050661e62cf664d7d44d7e0c
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-04-29 11:38:28 +0100

    client: rng skills tested and displaying

--- client/src/views/generate_form_view.js
+++ client/src/views/generate_form_view.js
@@ -26,13 +26,22 @@ class GenerateFormView {
       newChar.race = evt.target.race.value;
       newChar.job = evt.target.jobs.value;
       newChar.age = evt.target.age.value;
-      newChar.high_skill = 'Dexterity';
-      newChar.low_skill = 'Constitution';
+      newChar.high_skill = this.randomSkill();
+      newChar.low_skill = this.randomSkill();
+
+      console.log(newChar);
 
       const npc = new NPC();
       npc.postData(newChar);
     })
   }
+
+  randomSkill() {
+    const rng = new RNG();
+    const skill = rng.randArray(this.data.skills);
+    return skill.skill;
+  }
+
 }
 
 module.exports = GenerateFormView;
