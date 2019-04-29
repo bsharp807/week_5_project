04c77a1ee3b0e91aff62a45800484b9379697467
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-04-29 10:17:22 +0100

    updated css, updated npc url to take variables

--- client/src/models/npc.js
+++ client/src/models/npc.js
@@ -6,11 +6,12 @@ class NPC {
   constructor() {
 
     this.data = null;
+    this.id = 1;
 
   }
 
   getInitialDisplay () {
-    const url = 'http://localhost:3000/npc/1';
+    const url = `http://localhost:3000/npc/${this.id}`;
     const request = new RequestHelper(url);
     request.get()
       .then((data) => {
