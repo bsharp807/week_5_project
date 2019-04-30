5b716e89b34dc8f6d2f29a6b0f45e72e54bb924f
bsharp807 <a0030619@0030619s-MBP.home>
2019-04-30 22:16:14 +0100

    client: new functions added to random button

--- client/src/views/random_button_view.js
+++ client/src/views/random_button_view.js
@@ -16,12 +16,22 @@ class RandomButtonView {
       newChar.last_name = rand.filteredLastName(newChar.race);
       newChar.job = rand.randomJob();
       newChar.age = rand.randomAge();
+      newChar.trait = rand.randomTrait();
+      newChar.appearance = rand.randomAppearance();
+      newChar.talent = rand.randomTalent();
+      newChar.mannerism = rand.randomMannerism();
+      newChar.ideal = rand.randomIdeal();
+      newChar.bond = rand.randomBond();
+      newChar.birthplace = rand.randomBirthplace();
+      newChar.flaw = rand.randomFlaw();
+
       newChar.high_skill = rand.randomSkill();
       newChar.low_skill = rand.randomSkill();
       if (newChar.low_skill === newChar.high_skill) {
         newChar.low_skill = rand.randomSkill();
       }
 
+      console.log(newChar);
       const npc = new NPC();
       npc.postData(newChar);
     })
