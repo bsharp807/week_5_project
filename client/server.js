d609f546315d23570015549cf00fed68f11cedd7
bsharp807 <bsharp807@gmail.com>
2019-11-25 11:20:20 +0000

    changed port to 23456

--- client/server.js
+++ client/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+
+const port = 23456
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
-app.listen(3001, () => console.log(`NPC app listening on port 3001!`));
+app.listen(port, () => console.log(`NPC app listening on port ${port}!`));
