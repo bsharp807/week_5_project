06b1e5da91d2fae3a4cc995c5d115bd799a065fa
bsharp807 <bsharp807@gmail.com>
2019-11-25 10:56:31 +0000

    edited small details, updated npm

--- client/server.js
+++ client/server.js
@@ -4,4 +4,4 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
-app.listen(3001, () => console.log(`Example app listening on port 3001!`));
+app.listen(3001, () => console.log(`NPC app listening on port 3001!`));
